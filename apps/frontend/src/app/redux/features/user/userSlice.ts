3f167b42d7ca99927e39bb1363d6a3c1e6910501
Ryan Moon <ryanmoondevelopment@gmail.com>
2022-11-14 17:30:10 -0600

    updated

--- apps/frontend/src/app/redux/features/user/userSlice.ts
+++ apps/frontend/src/app/redux/features/user/userSlice.ts
@@ -1,5 +1,5 @@
 import {createSlice, PayloadAction} from "@reduxjs/toolkit"
-import {useAppSelector} from "../../hooks";
+import {useAppSelector} from "../../redux-hooks";
 
 export type UserValues ={
   isLoggedIn: boolean,
